069a6ab8b89027e0f3d9333b56bb7e7cf536e8c6
Jessil7 <145567068+Jessil7@users.noreply.github.com>
2024-09-02 08:40:26 -0300

    Cutscene

---
+++ Oficina2024/Assets/Pixel Adventure 1/Assets/Scripts/EfeitoDigitador.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class EfeitoDigitador : MonoBehaviour
+{
+   private TextMeshProUGUI componenteTexto;
+   private AudioSource _audioSource;
+}
